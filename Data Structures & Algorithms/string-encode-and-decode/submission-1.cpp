366ca38723fe2db76b1005996e1dd51a1a72179c
Shaoyu Fan <fanshaoyu9@outlook.com>
2026-08-10 10:00:47 -0400

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string result;
+
+        for (const string& str : strs) {
+            result += to_string(str.size());
+            result += '#';
+            result += str;
+        }
+
+        return result;
+    }
+
+    vector<string> decode(string s) {
+        vector<string> result;
+        int i = 0;
+
+        while (i < s.size()) {
+            int j = i;
+            while (s[j] != '#') {
+                j++;
+            }
+
+            int size = stoi(s.substr(i, j - i));
+            i = j + 1;
+            result.push_back(s.substr(i, size)); 
+            i += size;
+        }
+
+        return result;
+    }
+};
