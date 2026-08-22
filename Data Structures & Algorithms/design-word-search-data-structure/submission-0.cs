6eaf7f0c66e650f3c5fcef7c6b31f9dc6cbf65ca
Shaoyu Fan <fanshaoyu9@outlook.com>
2026-08-21 20:14:41 -0400

    Add: design-word-search-data-structure - submission-0

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-0.cs
@@ -0,0 +1,51 @@
+public class WordDictionary {
+    public class TrieNode {
+        public Dictionary<char, TrieNode> children = new Dictionary<char, TrieNode>();
+        public bool isEnd = false;
+    }
+
+    private TrieNode root;
+
+    public WordDictionary() {
+        root = new TrieNode();
+    }
+    
+    public void AddWord(string word) {
+        var cur = root;
+        foreach (char c in word) {
+            if (!cur.children.ContainsKey(c)) {
+                cur.children[c] = new TrieNode();
+            }
+            cur = cur.children[c]; 
+        }
+        cur.isEnd = true;
+    }
+    
+    public bool Search(string word) {
+        return Dfs(word, 0, root);
+    }
+
+    private bool Dfs(string word, int j, TrieNode node) {
+        var cur = node;
+
+        for (int i = j; i < word.Length; i++) {
+            char c = word[i];
+            if ( c == '.') {
+                foreach (TrieNode child in cur.children.Values) {
+                    if ( child != null && Dfs(word, i + 1, child)) {
+                        return true;
+                    }
+                }
+                return false;
+            } else {
+                if (!cur.children.ContainsKey(c)) {
+                    return false;
+                }
+            }
+
+            cur = cur.children[c];
+        }
+    
+        return cur.isEnd;
+    }
+}
