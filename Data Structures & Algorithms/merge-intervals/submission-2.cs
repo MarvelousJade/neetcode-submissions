1b48de40e147226e747a3e144b06b6c6344f33e6
Shaoyu Fan <fanshaoyu9@outlook.com>
2026-08-28 20:08:02 -0400

    Add: merge-intervals - submission-2

---
+++ Data Structures & Algorithms/merge-intervals/submission-2.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int[][] Merge(int[][] intervals) {
+        Array.Sort(intervals, (a, b) => a[0].CompareTo(b[0]));    
+
+        var output = new List<int[]>();
+        output.Add(intervals[0]);
+
+        foreach (int[] interval in intervals) {
+            int start = interval[0];
+            int end = interval[1];
+            int lastEnd = output[^1][1];
+
+            if(start <= lastEnd) {
+                output[^1][1] = Math.Max(lastEnd, end);
+            } else {
+                output.Add(interval);
+            }
+        }
+
+        return output.ToArray();
+    }
+}
